5a0355e47f116898804bc72f74e63884b77cba8f
haruki7049 <tontonkirikiri@gmail.com>
2025-07-19 09:23:44 +0900

    feat(repo): Init zig 0.14.1

---
+++ src/main.zig
@@ -0,0 +1,8 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+
+const std = @import("std");
+const lib = @import("io_lib");
+
+pub fn main() !void {}
